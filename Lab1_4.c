f21ed996fa30b55216199a30de912ab66ba00942
Oateat <87791729+0atest@users.noreply.github.com>
2023-12-13 22:16:06 +0700

    Update Lab1_4.c

--- Lab1_4.c
+++ Lab1_4.c
@@ -3,26 +3,26 @@
 จากคีย์บอร์ด ตามโปรโตไทป์ต่อไปนี้ */
 #include <stdio.h>
 
-int GetSet( int [] ) ;
+int GetSet(int []) ;
 
 int main() {
-    int *data, num ;
-    num = GetSet( &data ) ;
+    int *data, num;
+    num = GetSet(&data) ;
     return 0 ;
 }//end function
 
 int GetSet(int data[]) {
-    int num;
+    int num , i;
     printf("Get number of members: ");
-    scanf("%d", &num);
+    scanf("%d" , &num);
     
     for (int i = 0; i < num; i++) {
-        printf("Membership fee %d: ", i + 1);
-        scanf("%d", &data[i]);
+        printf("Membership fee %d: " , i + 1);
+        scanf("%d" , &data[i]);
     }
 
-    printf("Number of members: %d\n", num);
+    printf("Number of members: %d\n" , num);
     for (int i = 0; i < num; ++i) {
-        printf("Member %d : %d\n", i + 1, data[i]);
+        printf("Member %d : %d\n" , i + 1 , data[i]);
     }
 }
